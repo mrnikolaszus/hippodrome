d26e9f1846a4f62cc6a159061f7e09df279cf31d
Nikolas <9225407@gmail.com>
2023-06-25 14:16:12 +0300

    logging BUG

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,14 +21,14 @@ public class Main {
         Hippodrome hippodrome = new Hippodrome(horses);
         logger.info("Начало скачек. Количество участников: {}", horses.size());
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10 ; i++) {
             hippodrome.move();
             watch(hippodrome);
             TimeUnit.MILLISECONDS.sleep(200);
         }
 
         String winnerName = hippodrome.getWinner().getName();
-        System.out.println("Победил " + winnerName + "!");
+//        System.out.println("Победил " + winnerName + "!");
         logger.info("Окончание скачек. Победитель: {}", winnerName);
     }
 
